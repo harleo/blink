cc6de2021bc9d0220940ddb5fff5183a332c4228
Leonid <harleo@users.noreply.github.com>
2018-10-28 16:25:14 +0100

    Remove file type argument, Move main function closer to last call
    Specifying a different file type than PNG threw a warning from Selenium, opted to remove feature

--- blink.py
+++ blink.py
@@ -38,6 +38,54 @@ def output_handler(output_folder):
         return output_folder
 
 
+def process_urls(url_list, output_location, driver):
+    """
+    Process the list of URLs, visiting each page and saving a screenshot of the page to the target location directory.
+    :param url_list: list-like object of pages to visit
+    :param output_location: directory to store the screenshots in
+    :param driver: driver that is capable of GET'ing a URL and saving the picture of it
+    """
+    page_counter = 0
+    print(f"[:] Processing {len(url_list)} URL(s)")
+
+    for url in url_list:
+        page_counter += 1
+        print(f"[{page_counter}/{len(url_list)}] Opening {url}")
+
+        try:
+            driver.get("http://" + url)
+            driver.save_screenshot(output_location + "/" + url + ".png")
+
+        except WebDriverException as wde:
+            print(f"[!] Error retrieving web page '{url}'. Exception:\n{wde}")
+
+        except IOError:
+            print(f"[!] Couldn't save {url}, skipping...")
+
+    driver.quit()
+
+
+def url_list_from_file(input_file='example.txt'):
+    """
+    Read the specified file and return all lines inside the file, as a list of lines.
+    :param input_file: path to an input file
+    :return: list of lines in the file, e.g. URLs separated by new-line
+    """
+    return [line.rstrip() for line in open(input_handler(input_file), 'r')]
+
+
+def get_driver_options(window_size):
+    """
+    Default driver options, using the specified window size
+    :param window_size: the size of the browser window and effectively the screenshots
+    :return: options for the driver
+    """
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument(f"window-size={window_size}")
+    return options
+
+
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option(
     "-i",
@@ -74,26 +122,8 @@ def output_handler(output_folder):
     show_default=True,
     help="web page request timeout in seconds.",
 )
-@click.option(
-    "-f",
-    "--format",
-    "file_format",
-    type=str,
-    default="png",
-    show_default=True,
-    help="output file format.",
-)
-@click.option(
-    "-f",
-    "--format",
-    "file_format",
-    default="png",
-    show_default=True,
-    type=click.Choice(["png", "jpg"]),
-    help="set output file format: 'png' or 'jpg'."
-)
 @check_ssl
-def main(input_file, output_folder, window_size, time_out, file_format):
+def main(input_file, output_folder, window_size, time_out):
     """ Orchestrator for storing screenshots of web pages """
 
     output_location = output_handler(output_folder)
@@ -108,56 +138,8 @@ def main(input_file, output_folder, window_size, time_out, file_format):
 
     process_urls(url_list, output_location, driver)
 
-    print(f"[:] Done processing {input_file}")
+    print(f"[:] Done processing {input_handler(input_file)}")
 
-
-def process_urls(url_list, output_location, driver):
-    """
-    Process the list of URLs, visiting each page and saving a screenshot of the page to the target location directory.
-    :param url_list: list-like object of pages to visit
-    :param output_location: directory to store the screenshots in
-    :param driver: driver that is capable of GET'ing a URL and saving the picture of it
-    """
-    page_counter = 0
-    print(f"[:] Processing {len(url_list)} URL(s)")
-
-    for url in url_list:
-        page_counter += 1
-        print(f"[{page_counter}/{len(url_list)}] Opening {url}")
-
-        try:
-            driver.get("http://" + url)
-            driver.save_screenshot(output_location + "/" + url + "." + file_format)
-
-        except WebDriverException as wde:
-            print(f"[!] Error retrieving web page '{url}'. Exception:\n{wde}")
-
-        except IOError:
-            print(f"[!] Couldn't save {url}, skipping...")
-
-    driver.quit()
-
-
-def url_list_from_file(input_file='example.txt'):
-    """
-    Read the specified file and return all lines inside the file, as a list of lines.
-    :param input_file: path to an input file
-    :return: list of lines in the file, e.g. URLs separated by new-line
-    """
-    return [line.rstrip() for line in open(input_handler(input_file), 'r')]
-
-
-def get_driver_options(window_size):
-    """
-    Default driver options, using the specified window size
-    :param window_size: the size of the browser window and effectively the screenshots
-    :return: options for the driver
-    """
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-    options.add_argument(f"window-size={window_size}")
-    return options
-
-
+    
 if __name__ == "__main__":
     main()
